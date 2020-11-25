c247ae2cea30ec9363e9f6e378d44454e87dbf1d
Andi Zeneli <azeneli2000@gmail.com>
2020-11-25 12:23:33 +0100

    ndryshime vl

--- index.js
+++ index.js
@@ -3,45 +3,60 @@ const app = express();
 const path = require('path');
 const crudRedis = require('./model')
 const redis = require('redis');
+
+//redis
 const client = redis.createClient('redis://localhost:6379');
-
-
+//ejs
 app.set("view engine", "ejs");
+//path
 app.use(express.static(path.join(__dirname, "/")));
 
-crudRedis.add('a',
+client.hset("3", "emri", "zenel" , "mbiemmri" , "Zeneli",(err,reply)=>{console.log(reply)} );
+//client.set('all', JSON.stringify([]));
+var a= client.scan(0,(err,val)=>{client.hgetall(val[1][1]),(err,val)=>console.log(val) });
+console.log(a);
+// adding new girl
+ 
+crudRedis.add('all',
   {
-    id: 1,
-    name: 'eva',
-    years: '30',
-    hobby: 'dance',
-    height: 175,
-    weight: 60,
-    chest: 2,
-    priceNight: 1000,
-    priceHour: 100,
-    priceDay: 2000,
-    aboutMe: 'kasdjhfkjsdhfalsdh dasfkjadsfja sdjfdjasgfjhsgfa sljhfgdkjasgfkjdhasgfkjhagsd sdfjhgdasjhgfkjhasdgfsdkjgfas',
-    profilePhotoPath: 'img/girl.jpg',
-    photo1Path: '',
-    photo2path: '',
-    photo3Path: ''
+      id: 3,
+      name: 'eva',
+      years: '30',
+      hobby: 'dance',
+      height: 175,
+      weight: 60,
+      chest: 2,
+      priceNight: 1000,
+      priceHour: 100,
+      priceDay: 2000,
+      aboutMe: 'kasdjhfkjsdhfalsdh dasfkjadsfja sdjfdjasgfjhsgfa sljhfgdkjasgfkjdhasgfkjhagsd sdfjhgdasjhgfkjhasdgfsdkjgfas',
+      profilePhotoPath: 'img/girl2.jpg',
+      photo1Path: '',
+      photo2path: '',
+      photo3Path: ''
 
-  });
-var s;
+    });
 
 
+//getting all girls
 app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/:id", (req, res) => { 
+client.get("all", (err, val) => {
+      var allGirls = JSON.parse(val);
+      res.render("home", {allGirls : allGirls});
+    });
+  })
+//serving model with particular id
+app.get("/:id", (req, res) => {
   var idG = req.params.id;
-  client.get(idG,(err,val)=>{
-    var json = JSON.parse(val);
-     
-    res.render("girl",{json:json});
-    })
-  
+  client.get("all", (err, val) => {
+    var jsonArray = JSON.parse(val).filter((obj)=>{ return obj.id==idG} );
+    console.log(jsonArray);
+    var json = jsonArray[0];
+    console.log(json)
+   // console.log(jsonArray[0])
+    res.render("girl", { json: json });
+  })
+
 });
 app.listen(3000, () => {
   console.log("server started on port 3000");
--- model.js
+++ model.js
@@ -1,8 +1,15 @@
 
+const { json } = require('express');
 const redis = require('redis');
 const client = redis.createClient('redis://localhost:6379');
+
+
 exports.add = function (key, value) {
-    client.set(key, JSON.stringify(value))
+   client.get("all",(err,val)=>{
+    var json = JSON.parse(val);
+     json.push(value);
+    client.set(key, JSON.stringify(json));
+   });
 
 
 }
